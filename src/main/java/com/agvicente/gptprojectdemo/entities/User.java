1cea3d36d5fb29faec85e34c4234cf81ad5704d7
Augusto Custodio <custodioovicente@outlook.com.br>
2023-06-17 19:07:57 -0300

    Adding JPA mapping on User entity

--- src/main/java/com/agvicente/gptprojectdemo/entities/User.java
+++ src/main/java/com/agvicente/gptprojectdemo/entities/User.java
@@ -1,18 +1,32 @@
 package com.agvicente.gptprojectdemo.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "phone")
     private String phone;
+
+    @Column(name = "password")
     private String passord;
 
-    private User(){
+    public User(){
 
     }
 
