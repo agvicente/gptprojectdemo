5ebfc56adfad59e1e2ecd576df6814967b157ec8
Augusto Custodio <custodioovicente@outlook.com.br>
2023-06-17 18:43:57 -0300

    Creating User entity

---
+++ src/main/java/com/agvicente/gptprojectdemo/resources/UserResources.java
@@ -0,0 +1,18 @@
+package com.agvicente.gptprojectdemo.resources;
+
+import com.agvicente.gptprojectdemo.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "31994596651", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
