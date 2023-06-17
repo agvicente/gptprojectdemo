02dbdd7e3abf98c23867fcc436364881c1a10dde
Augusto Custodio <custodioovicente@outlook.com.br>
2023-06-16 21:23:23 -0300

    Projeto Arquitetura Java (Initial Commit)

---
+++ src/main/java/com/agvicente/gptprojectdemo/GptprojectdemoApplication.java
@@ -0,0 +1,13 @@
+package com.agvicente.gptprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptprojectdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptprojectdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agvicente/gptprojectdemo/GptprojectdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agvicente.gptprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptprojectdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
